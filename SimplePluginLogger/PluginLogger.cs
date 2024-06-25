0e4d520a9f2bae976c99dca6bdf4cbcb8930bbd7
Alexander Valiev <64912185+NeosMegas@users.noreply.github.com>
2024-06-25 19:45:57 +0500

    Some comments added

--- SimplePluginLogger/PluginLogger.cs
+++ SimplePluginLogger/PluginLogger.cs
@@ -9,12 +9,19 @@ namespace SimplePluginLogger
         private static string logFilePath = Path.GetTempFileName();
         public static string LogFilePath { get => logFilePath; }
 
+        /// <summary>
+        /// Запись сообщения лога во временный файл
+        /// </summary>
+        /// <param name="message">Сообщение</param>
         public static void Log(string message)
         {
             using (StreamWriter sr = File.AppendText(logFilePath))
                 sr.WriteLine(DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss.fff ") + message);
         }
 
+        /// <summary>
+        /// Открыть временный файл (например, в блокноте)
+        /// </summary>
         public static void OpenLogFile() => Process.Start(logFilePath);
     }
 }
